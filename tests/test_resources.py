3afbda28203ab7beb42ecde279befa27d3664ebd
Philip Delorenzo <git@deloarts.com>
2022-12-02 13:44:43 +0100

    Allow same user id with different logon

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -54,7 +54,6 @@ def test_users():
     from pytia_bounding_box.resources import resource
 
     logon_list = []
-    id_list = []
 
     for user in resource.users:
         assert isinstance(user.logon, str)
@@ -62,10 +61,8 @@ def test_users():
         assert isinstance(user.name, str)
         assert isinstance(user.mail, str)
         assert user.logon not in logon_list
-        assert user.id not in id_list
 
         logon_list.append(user.logon)
-        id_list.append(user.id)
 
 
 def test_props():
@@ -96,6 +93,7 @@ def test_modifier_filter():
     import re
 
     from pytia.const import USERNAME
+
     from pytia_bounding_box.resources import resource
 
     user = resource.get_user_by_logon(USERNAME)
