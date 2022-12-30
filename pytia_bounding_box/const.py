f79a1dd111471b386e3c2711f97aa50ab15976c5
Philip Delorenzo <git@deloarts.com>
2022-12-30 13:26:15 +0100

    Use pathlib.Path for venv folder

--- pytia_bounding_box/const.py
+++ pytia_bounding_box/const.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 import os
 from enum import Enum
+from pathlib import Path
 
 __version__ = "0.3.0"
 
@@ -23,8 +24,8 @@ LOG = "app.log"
 PID = os.getpid()
 PID_FILE = f"{TEMP}\\{PYTIA_BOUNDING_BOX}.pid"
 VENV = f"\\.env\\{APP_VERSION}"
-VENV_PYTHON = VENV + "\\Scripts\\python.exe"
-VENV_PYTHONW = VENV + "\\Scripts\\pythonw.exe"
+VENV_PYTHON = Path(VENV, "Scripts\\python.exe")
+VENV_PYTHONW = Path(VENV, "Scripts\\pythonw.exe")
 PY_VERSION = APPDATA + "\\pyversion.txt"
 
 CONFIG_APPDATA = "config.json"
